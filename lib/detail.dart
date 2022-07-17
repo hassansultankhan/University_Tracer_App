c7dc38d645e904033a5313a059890dbfb12ac5cd
Hassan Sultan Khan <hassansultankhan@gmail.com>
2022-07-17 15:59:59 -0700

    17/07/22

---
+++ lib/detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+import 'package:universitylist_app/universtiy.dart';
+
+class details extends StatelessWidget {
+  university universitydetail;
+  details(this.universitydetail);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,8 @@
 import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:universitylist_app/detail.dart';
 import 'package:universitylist_app/universtiy.dart';
-
 
 class homescreen extends StatefulWidget {
   homescreen({Key? key}) : super(key: key);
@@ -15,78 +13,75 @@ class homescreen extends StatefulWidget {
 
 class _homescreenState extends State<homescreen> {
   var country = "Pakistan";
-   List<university> unidata = [];
+  List<university> unidata = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar:  AppBar(
-        backgroundColor: Colors.white,
-        title: const Text('Univerity Tracer'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-
-
-      body: ListView(
-        
-        
-       children: [
-        SizedBox(height: 5,),
-        ElevatedButton(onPressed: ()=>getdata(), child: Text('GET DATA'), ),
-        SizedBox(height: 5,),
-        ListView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          itemCount: unidata.length,
-          itemBuilder: (context, index){
-          
-          return Column(
-            
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-               
-             
-              child: Row( 
-                
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                
-                children:
-                [ 
-                Container(
-                width: MediaQuery.of(context).size.width,
-                child:Padding(padding:EdgeInsets.only(left: 10),
-                  child: Text('${unidata[index].name}\nState/Province: ${unidata[index].state}', textAlign: TextAlign.left,),
-                ),
-                ),
-                ]
-              ),
-               ),
-                Container(
-                  
-                              height: 5,
-                             decoration: BoxDecoration(color: Color.fromARGB(255, 200, 150, 135))
-                            ),
-            ],
-
-          );
-        }
-        )
-       ],
-      ) 
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text('Univerity Tracer'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: ListView(
+          children: [
+            SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(
+              onPressed: () => getdata(),
+              child: Text('GET DATA'),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: unidata.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      InkWell(
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: 10),
+                                    child: Text(
+                                      '${unidata[index].name}\nState/Province: ${unidata[index].state}',
+                                      textAlign: TextAlign.left,
+                                    ),
+                                  ),
+                                ),
+                              ]),
+                        ),
+                        onTap: () => details(),
+                      ),
+                      Container(
+                          height: 5,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 200, 150, 135))),
+                    ],
+                  );
+                })
+          ],
+        ));
   }
 
-  getdata()async{
-    var url = Uri.parse('http://universities.hipolabs.com/search?country=Pakistan');
-    var response =await  http.get(url);
-    List <dynamic> data = jsonDecode(response.body);
+  getdata() async {
+    var url =
+        Uri.parse('http://universities.hipolabs.com/search?country=Pakistan');
+    var response = await http.get(url);
+    List<dynamic> data = jsonDecode(response.body);
 
-  setState(() {
+    setState(() {
       unidata = university.getuniveritydata(data);
     });
-    
   }
 }
